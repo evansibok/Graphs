7a756cd1332aa91e71df64ec31125183eabce0b9
Evans Ibok <evansibok@gmail.com>
2020-06-01 20:28:02 +0100

    add bft and dft

---
+++ projects/ancestor/sum.py
@@ -0,0 +1,14 @@
+
+def simpleArraySum(ar):
+    sum = 0
+    for i in ar:
+        sum = sum + i
+        # print(sum)
+    print(sum)
+
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+s_a_s = simpleArraySum(numbers)
+
+print(s_a_s)
